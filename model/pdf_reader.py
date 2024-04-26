10a9e87d80779206593033405efe1cfcfe654ffb
FMariaD <71096257+FMariaD@users.noreply.github.com>
2024-04-26 06:46:25 +0300

    commentaries

--- model/pdf_reader.py
+++ model/pdf_reader.py
@@ -4,6 +4,12 @@ from PyPDF2 import PdfReader
 
 
 def download_file(url):
+    """
+    считывает текст с pdf-файла по url
+    возвращает текст статьи в формате str
+
+    :url: - str, ссылка
+    """
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Windows; Windows x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
     }
@@ -21,6 +27,10 @@ def download_file(url):
 
 
 def preprocess(text):
+    """
+    убирает перенос слов на новую строку и разбиение на абзацы
+    """
+    
     text1 = text.replace("-\n", "")
     text2 = text1.replace("\n", " ")
     return text2
