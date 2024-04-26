fdf97ebafcc887347ab80ddab5e5e948fafbebac
Maximilean <71936925+Maximilean@users.noreply.github.com>
2024-04-26 06:26:36 +0300

    Add comments

--- summary.py
+++ summary.py
@@ -4,24 +4,28 @@ from langchain_core.documents.base import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def summary(sum_llm, text):
+	""" Return summary of giving text """
+
+	# Prompts for summary
 	map_prompt = load_prompt('lc://prompts/summarize/map_reduce/map.yaml')
 	combine_prompt = load_prompt('lc://prompts/summarize/map_reduce/combine.yaml')
 
-	documents = Document(page_content=text, metadata={"source": "local"})
-
+	documents = Document(page_content=text, metadata={"source": "local"}) # Convert text to langchain Document
+	
 	text_splitter = RecursiveCharacterTextSplitter(
 		chunk_size = 7000,
 		chunk_overlap  = 0,
 		length_function = len,
 		is_separator_regex = False
 	)
-	documents = text_splitter.split_documents([documents])
+	documents = text_splitter.split_documents([documents]) # Text splitter 
 	sum_chain = load_summarize_chain(sum_llm, chain_type="map_reduce", 
                              map_prompt=map_prompt,
                              combine_prompt=combine_prompt,
-                             verbose=False)
+                             verbose=False
+	)
 	
-	summary = sum_chain.invoke({"input_documents": documents})
+	summary = sum_chain.invoke({"input_documents": documents}) # Taking summary
 	
 	return summary 
 
